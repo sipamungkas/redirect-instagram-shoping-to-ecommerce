777dd634d4f05294e572c0096f4d4cb1b393c3a3
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-01-27 08:41:18 +0700

    init commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var qs = (function (a) {
+  if (a == "") return {};
+  var b = {};
+  for (var i = 0; i < a.length; ++i) {
+    var p = a[i].split("=", 2);
+    if (p.length == 1) b[p[0]] = "";
+    else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+  }
+  return b;
+})(window.location.search.substr(1).split("&"));
+
+document.addEventListener("DOMContentLoaded", function () {
+  var url = qs["redirect"];
+  if (url !== "") {
+    return (window.location.href = url);
+  }
+  window.alert("invalid url");
+});
